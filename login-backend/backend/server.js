916a590e5b10c850bd765ebdf7f69d87bee1ad70
ErRahul001 <91515298+ErRahul001@users.noreply.github.com>
2023-03-25 14:42:16 +0530

    Add files via upload

---
+++ login-backend/backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import articleRoutes from "./routes/articleRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+
+dotenv.config();
+await connectDB();
+const app = express();
+
+app.use(express.json());
+app.use("/api/articles", articleRoutes);
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+    res.send("API is running");
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 2002;
+
+app.listen(PORT);
+console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
